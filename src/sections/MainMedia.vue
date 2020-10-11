80554259f5f1ca79b2899fdd42ac56ec229c4eb2
joker506 <joker506@mail.ru>
2020-10-12 01:45:13 +0300

    q

--- src/sections/MainMedia.vue
+++ src/sections/MainMedia.vue
@@ -3,7 +3,7 @@
     <div class="main ">
       <img class='ellipse' src="../assets/Ellipse.svg" alt="">
       <div
-        class="w-100 main__wrap container-xl d-flex justify-content-center flex-column"
+        class="w-100 main__wrap container-xl d-flex justify-content-start justify-content-lg-center flex-column"
       >
       <!-- Group.svg -->
         <img class='dots' src="../assets/Group.svg" alt="">
@@ -15,8 +15,8 @@
         <!-- </div> -->
 
         <!-- <div class="main__wrapp d-flex container-fluid"> -->
-        <div class="container-fluid d-flex justify-content-center">
-          <ul class="main__items">
+        <div class="container-fluid d-flex flex-lg-row flex-column justify-content-center">
+          <ul class="main__items d-flex d-sm-block justify-content-between flex-wrap">
             <li class="main__item" v-for="(item, index) in items" :key="index">
               {{ item.title }}
             </li>
@@ -31,6 +31,7 @@
             организация фестиваля "Точка <br />
             Будущего" раз в год
           </p>
+           
         </div>
         <!-- </div> -->
       </div>
@@ -106,7 +107,8 @@ export default {
 .main__items {
   list-style: none;
   position: relative;
-  &::before {
+   @media(min-width:768px) {
+   &::before {
     content: '';
     position: absolute;
     width: 251px;
@@ -116,12 +118,12 @@ export default {
     width: 251px;
     mix-blend-mode: multiply;
     border: 1px solid #6fcf97;
-  }
+  }}
 }
 .main__item {
   height: 32px;
   padding-top: 3px;
-  width: 276px;
+  //width: 276px;
   margin-right: 32px;
   margin-left: 32px;
   margin-bottom: 32px;
@@ -131,10 +133,26 @@ export default {
   font-size: 18px;
   line-height: 32px;
   color: #000000;
+/*   @media(max-width:567px) {
+     
+    &::before {
+    
+    content: '';
+    position: absolute;
+    width: 16px;
+    height: 0px;
+    top: 14px;
+   // width: 251px;
+    mix-blend-mode: multiply;
+    border: 1px solid #6fcf97;
+    } }
+     */
+  
   &:hover {
     cursor: pointer;
     color: #219653;
   }
+   
 }
 .main__content {
   font-family: 'San-Francisco', sans-serif;
@@ -143,5 +161,62 @@ export default {
   font-size: 44px;
   line-height: 56px;
   color: #000000;
+}
+
+@media (max-width: 375px) {
+  .main{
+    background-image: url('~@/assets/backgroundMainES.svg');
+    background-size: cover;
+    background-position: 50% 37%;
+   
+  }
+  .main-wrapper{
+    padding-top: 60px;
+  }
+  .main__wrap{
+    height: 520px;
+  }
+  .main__title{
+  margin-top: 50px;
+  margin-bottom: 40px;
+font-style: normal;
+font-weight: bold;
+font-size: 31px;
+line-height: 120%;
+  }
+  .ellipse{
+    width: 124px;
+    height: 124px;
+  }
+  .main .ellipse{
+    top: -3%;
+    left: -9%;
+  }
+  .main__items{
+    //width: 345px;
+    padding: 0;
+   
+  }  
+  .main__item{
+    display: block;
+  padding-top: 0px;
+    margin: 0;
+font-size: 14px;
+line-height: 32px;
+  }
+  .btn{
+    position: relative;
+    top: 245px;
+    left: -176px;
+  }
+br{
+  display: none;
+}
+  .main__content{
+    font-weight: 600;
+font-size: 24px;
+line-height: 32px;
+  }
+  
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath:':/router-view/'
+    publicPath:'/router-view/'
   }

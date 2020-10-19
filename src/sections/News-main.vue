9e2754b6ebcc91c2600aae6f43226801483ff995
joker506 <joker506@mail.ru>
2020-10-19 22:15:43 +0300

    correcr toggle

--- src/sections/News-main.vue
+++ src/sections/News-main.vue
@@ -1,174 +1,188 @@
 <template>
-  <div class='News-main '>
-    <img class='dots' src="../assets/news-dots.svg" alt="">
+  <div class="News-main ">
+    <img class="dots" src="../assets/news-dots.svg" alt="" />
     <div class="container-xl news-wrapper">
       <div class="row justify-content-between">
-        <div class='col-lg-2 col-12'>
+        <div class="col-lg-2 col-12">
           <h3>Новости</h3>
         </div>
         <div class="col-lg-10 col-12 d-flex justify-content-around flex-wrap">
-          <div v-for="(article, index) in info" :key="index" class='col-lg-5 col-12 news-item'>
-            
-            <router-link 
-            v-if="index <= 1" class='d-flex flex-column align-items-end w-100 news-link'
-            :to="/nNewsArtic/ + index"
+          <div
+            v-for="(article, index) in info"
             :key="index"
+            class="col-lg-5 col-12 news-item"
+          >
+            <router-link
+              v-if="index <= 1"
+              class="d-flex flex-column align-items-end w-100 news-link"
+              :to="/nNewsArtic/ + index"
+              :key="index"
             >
-              <img class='w-98' v-bind:src="article.picture" alt="">
-              <span class="w-98 date">{{article.date}}</span>
-              <p class='w-98'>
-                {{article.title}}
+              <img class="w-98" v-bind:src="article.picture" alt="" />
+              <span class="w-98 date">{{ article.date }}</span>
+              <p class="w-98">
+                {{ article.title }}
               </p>
             </router-link>
           </div>
 
-          <div class='all-news-link col-5 pl-0'>
-          <router-link to="/News" >
+          <div class="all-news-link col-5 pl-0">
+            <router-link to="/News">
               Все новости
-              <svg width="44" height="20" viewBox="0 0 44 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 10H43M43 10L35.6111 1M43 10L35.6111 19" stroke="#219653" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <svg
+                width="44"
+                height="20"
+                viewBox="0 0 44 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 10H43M43 10L35.6111 1M43 10L35.6111 19"
+                  stroke="#219653"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
               </svg>
-          </router-link>
+            </router-link>
           </div>
-          <div class='col-5'></div>
+          <div class="col-5"></div>
         </div>
-
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex';
 export default {
-  name: "News-main",
+  name: 'News-main',
   data() {
     return {
       News: [
         {
-          img: require("../assets/news-item.png"),
+          img: require('../assets/news-item.png'),
           date: '15 фераля 2020',
-          paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est a auctor ut in maecenas id. Sollicitudin egestas id quis sed viverra ut pellentesque.',
-          url: '#'
+          paragraph:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est a auctor ut in maecenas id. Sollicitudin egestas id quis sed viverra ut pellentesque.',
+          url: '#',
         },
         {
-          img: require("../assets/news-item.png"),
+          img: require('../assets/news-item.png'),
           date: '20 фераля 2020',
-          paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est a auctor ut in maecenas id. Sollicitudin egestas id quis sed viverra ut pellentesque.',
-          url: '#'
-        }
-      ]
-    }
+          paragraph:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est a auctor ut in maecenas id. Sollicitudin egestas id quis sed viverra ut pellentesque.',
+          url: '#',
+        },
+      ],
+    };
   },
-  computed:{
-      ...mapState({
-        info: state => state.newsInfo.NewsArticle
-      }),
-    },
-}
+  computed: {
+    ...mapState({
+      info: state => state.newsInfo.NewsArticle,
+    }),
+  },
+};
 </script>
 
-<style lang='scss'>
-  
-  $animation-duration: .4s ease;
+<style lang="scss">
+$animation-duration: 0.4s ease;
 
-  .News-main{
-    padding: 5vh 0;
-    background-color:#F2F4F9;
-    position: relative;
-    .dots{
-      position: absolute;
-      top: 0;
-      right: 0;
-    }
-    h3{
-      font-weight: 600;
-      font-size: 40px;
-      line-height: 56px;
-      color: #000000;
-    }
-    a:hover{
-      text-decoration: none;
-    }
+.News-main {
+  padding: 5vh 0;
+  background-color: #f2f4f9;
+  position: relative;
+  .dots {
+    position: absolute;
+    top: 0;
+    right: 0;
   }
-
-  .news-link{
-    &:hover{
-      p{
-        color: #219653;
-        transition: color $animation-duration;
-      }
-    }
+  h3 {
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 56px;
+    color: #000000;
   }
+  a:hover {
+    text-decoration: none;
+  }
+}
 
-  .all-news-link{
-    padding-top: 56px;
-    a{
-      font-style: normal;
-      font-weight: bold;
-      font-size: 16px;
-      line-height: 32px;
+.news-link {
+  &:hover {
+    p {
       color: #219653;
-      letter-spacing: 0.1em;
-      text-transform: uppercase;
       transition: color $animation-duration;
-      &:hover{
-        color: darken($color: #219653, $amount: 10);
-        transition: color $animation-duration;
-      }
+    }
+  }
+}
+
+.all-news-link {
+  padding-top: 56px;
+  a {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 32px;
+    color: #219653;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    transition: color $animation-duration;
+    &:hover {
+      color: darken($color: #219653, $amount: 10);
+      transition: color $animation-duration;
+    }
+  }
+}
+
+.news-wrapper {
+  height: 95кvh;
+}
+
+.news-item {
+  border-left: 4px solid #66b42f;
+  span {
+    color: #000000;
+    font-size: 16px;
+    line-height: 24px;
+    margin: 16px 0;
+  }
+  p {
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 38px;
+    letter-spacing: 0.02em;
+    color: #000000;
+    transition: color $animation-duration;
+  }
+}
+
+@media (max-width: 992px) {
+  .News-main {
+    height: auto;
+    h3 {
+      margin-bottom: 25px;
     }
   }
 
-  .news-wrapper{
-    height: 95кvh;
+  .news-wrapper {
+    height: auto;
   }
 
-  .news-item{
-    border-left: 4px solid #66B42F;
-    span{
-      color: #000000;
-      font-size: 16px;
-      line-height: 24px;
-      margin: 16px 0;
-    }
-    p{
+  .all-news-link {
+    display: none;
+  }
+
+  .news-item {
+    p {
       font-weight: 600;
-      font-size: 32px;
-      line-height: 38px;
-      letter-spacing: 0.02em;
-      color: #000000;
-      transition: color $animation-duration;
+      font-size: 24px;
+      line-height: 32px;
     }
   }
 
-  @media (max-width: 992px){
-
-    .News-main{
-      height: auto;
-      h3{
-        margin-bottom: 25px;
-      }
-    }
-
-    .news-wrapper{
-      height: auto;
-    }
-
-    .all-news-link{
-      display: none;
-    }
-
-    .news-item{
-      p{
-        font-weight: 600;
-        font-size: 24px;
-        line-height: 32px;
-      }
-    }
-
-    .news-item:nth-child(2){
-      display: none;
-    }
+  .news-item:nth-child(2) {
+    display: none;
   }
+}
 </style>
--- src/views/Leaders.vue
+++ src/views/Leaders.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='leaders_page' id="app">
+  <div class="leaders_page" id="app">
     <div class="container-fluid p-0">
       <div class="title d-flex align-items-center justify-content-start">
         <div class="green"></div>
@@ -10,106 +10,133 @@
       <div class="container content">
         <div class="col-3">
           <div class="select">
-            <div 
-            v-for='(category,i) in leadersCategories' 
-            :key="category.type" 
-            class='w-100'>
-            <button v-if="category.active==true" class="active w-100">{{category.label}}</button>
-            <button v-on:click='ActivateCategory($event, i)' v-else class='w-100'>{{category.label}}</button>
+            <div
+              v-for="(category, index) in leadersCategories"
+              :key="category.type"
+              class="w-100"
+            >
+              <button
+                v-bind:class="{ active: activeIndex === index }"
+                @click="onClickBlock(index)"
+                class="w-100 mb-3"
+              >
+                {{ category.label }}
+              </button>
+
+              <!--  <button v-if="category.active == true" class="active w-100">
+                {{ category.label }}
+              </button> 
+              <button
+                v-on:click="ActivateCategory($event, i)"
+                v-else
+                class="w-100"
+              >
+                {{ category.label }}
+              </button> -->
             </div>
           </div>
         </div>
-        <div class="col-9">
-
-        </div>
+        <div class="col-9"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex';
 // import Header from '../components/Header';
 // import Footer from '../components/Footer';
 export default {
   name: 'Leaders',
+  data() {
+    return {
+      activeIndex: -1,
+    };
+  },
   components: {
     // Header, Footer
   },
-  computed:{
+  computed: {
     ...mapState({
-      leadersCategories: state => state.leadersInfo.categories
-    })
+      leadersCategories: state => state.leadersInfo.categories,
+    }),
   },
   methods: {
+    onClickBlock(index) {
+      if (this.activeIndex == index) {
+        this.activeIndex = -1;
+      } else {
+        this.activeIndex = index;
+      }
+    },
+
     ActivateCategory($event, i) {
-      this.leadersCategories[i].active = true
-      for (let j = 1; j < 20; j++){
-        this.leadersCategories[i+j].active = false
+      this.leadersCategories[i].active = true;
+      for (let j = 1; j < 20; j++) {
+        this.leadersCategories[i + j].active = false;
       }
-      for (let j = 20; j>-1; j--){
-        this.leadersCategories[i-j].active = false
+      for (let j = 20; j > -1; j--) {
+        this.leadersCategories[i - j].active = false;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  @import '../scss/variables';
-  $distance: 250px;
+@import '../scss/variables';
+$distance: 250px;
 
-  .green{
-    width: $distance;
-    height: 8px;
-    content: '';
-    z-index: 1;
-    background-color: $base-green;
+.green {
+  width: $distance;
+  height: 8px;
+  content: '';
+  z-index: 1;
+  background-color: $base-green;
+}
+
+.white {
+  background: #ffffff;
+  box-shadow: 20px 24px 32px rgba(108, 137, 164, 0.15);
+  width: 733px;
+  margin-left: calc(-#{$distance}/ 2);
+  padding: 26px 0;
+  height: 140px;
+  position: relative;
+  h2 {
+    font-weight: bold;
+    font-size: 80px;
+    line-height: 88px;
+    letter-spacing: 2px;
+    color: #1c1642;
+    position: absolute;
+    right: -145px;
   }
+}
 
-  .white{
-    background: #FFFFFF;
-    box-shadow: 20px 24px 32px rgba(108, 137, 164, 0.15);
-    width: 733px;
-    margin-left: calc(-#{$distance}/2);  
-    padding: 26px 0;
-    height: 140px;
-    position: relative;
-    h2{
+.leaders_page {
+  background-color: $base-bg-news;
+  .content {
+    margin-top: 40px;
+  }
+  .select {
+    padding: 16px;
+    background-color: #fff;
+    border-radius: 8px;
+    button {
+      padding: 16px 12px;
+      background: none;
+      color: #1c1642;
+      font-size: 20px;
+      line-height: 24px;
       font-weight: bold;
-      font-size: 80px;
-      line-height: 88px;
-      letter-spacing: 2px;
-      color: #1C1642;
-      position: absolute;
-      right: -145px;
-    } 
-  }
-
-  .leaders_page{
-    background-color: $base-bg-news;
-    .content{
-      margin-top: 40px;
     }
-    .select{
-      padding: 16px;
-      background-color: #fff;
+
+    .active {
+      background: #2f80ed;
+      color: #fff;
       border-radius: 8px;
-      button{
-        padding: 16px 12px;
-        background: none;
-        color: #1C1642;
-        font-size: 20px;
-        line-height: 24px;
-        font-weight: bold;
-      }
-
-      .active{
-        background: #2F80ED;
-        color: #fff;
-        border-radius: 8px;
-      }
     }
   }
-
+}
 </style>

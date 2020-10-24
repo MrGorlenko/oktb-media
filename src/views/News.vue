95dfd684ed2f43f0aca19015b47e8e964885df5c
joker506 <joker506@mail.ru>
2020-10-24 15:13:49 +0300

    add search Video

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="news">
-  <router-view></router-view>
-  <Search />
-  <NewsCard />
-  
+    <div class="container">
+      <h2 class="search__news mb-4">Новости</h2>
+    </div>
+    <router-view></router-view>
+    <Search />
+    <NewsCard />
   </div>
 </template>
 
 <script>
-import Search from '@/components/Search'
-import NewsCard from '@/components/News-Card'
+import Search from '@/components/Search';
+import NewsCard from '@/components/News-Card';
 
 export default {
   name: 'News',
   components: {
-  // Header,
-  Search,
-  NewsCard
-    },
+    // Header,
+    Search,
+    NewsCard,
+  },
 };
 </script>
 
 <style lang="scss" scope>
 @import '../scss/variables';
-  .news{
-    background-color: $base-bg-news;
-    padding-bottom: 10px;
-  }
+.news {
+  background-color: $base-bg-news;
+  padding-bottom: 10px;
+}
 
-  @media (max-width: 376px) {
-    .news{
-        background-color: #ffffff;
-    }
+@media (max-width: 376px) {
+  .news {
+    background-color: #ffffff;
+  }
 }
 </style>

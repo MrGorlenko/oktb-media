866de5989c788b6592d934878aa11194f47c0793
Ulandt <ulanbek.dt94@gmail.com>
2020-10-25 13:41:58 +0600

    news data

--- src/components/News-Card.vue
+++ src/components/News-Card.vue
@@ -1,42 +1,47 @@
 <template>
-  <div class="search container-xl align-items-center">  
+  <div class="search container-xl align-items-center">
     <div class="container-lg d-flex flex-column align-items-end">
-      <InfoCard 
-      v-for="(article, index) in book" :key="index"
-      :title=article.title
-      :author=article.author
-      :date=article.date 
-      :path='/nNewsArtic/'
-      :index=index
-      :picture=article.picture
+      <InfoCard
+          v-for="(article, index) in newsCard" :key="index"
+          :title=article.title
+          :author=article.author
+          :date=article.date
+          :path='/nNewsArtic/'
+          :index=index
+          :picture=article.picture
       >
       </InfoCard>
     </div>
-  </div> 
+  </div>
 </template>
 
 <script>
-import {mapState,mapGetters} from 'vuex'
+import {mapState} from 'vuex'
 import InfoCard from '@/components/subcomponents/InfoCard.vue'
 
 export default {
-    name:"News_Card",
-    data() {
-      return {
-      }
-
-    },
-    components: {
-      InfoCard
-    },
-    computed:{
-      ...mapState({
-        info: state => state.newsInfo.NewsArticle
-      }),
-      ...mapGetters({
-        book:'newsInfo/filterNews'
-      })
-    },
+  name: "News_Card",
+  data() {
+    return {
+      newsCard: []
+    }
+  },
+  async created() {
+    var response = await fetch('http://localhost:8000/api/news')
+    this.newsCard = await response.json();
+    console.log(this.newsCard)
+  },
+  components: {
+    InfoCard
+  },
+  computed: {
+    ...mapState({
+      info: state => state.newsInfo.NewsArticle
+    }),
+    // ...mapGetters({
+    //   book:'newsInfo/filterNews'
+    // })
+  },
 
 }
 </script>

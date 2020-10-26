4b59051b96ec6442b0dbb3abb9455f947f0f6fea
Ulandt <ulanbek.dt94@gmail.com>
2020-10-27 03:36:33 +0600

    minor bug fixes

--- oktb_backend/articles/models.py
+++ oktb_backend/articles/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Theme(models.Model):
     name = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
